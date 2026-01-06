1bc210e5cc43b5ba5bd3042e032f213dbedf8f95
만재 신 <tlsakswo97@dongguk.edu>
2026-01-06 18:51:18 +0900

    초기 커밋.

---
+++ LN_Console_20260106Dlg.h
@@ -0,0 +1,34 @@
+﻿
+// LN_Console_20260106Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CLNConsole20260106Dlg 대화 상자
+class CLNConsole20260106Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CLNConsole20260106Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LN_CONSOLE_20260106_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
---
+++ resource.h
@@ -0,0 +1,50 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// LNConsole20260106.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_LN_CONSOLE_20260106_DIALOG  102
+#define IDR_MAINFRAME                   128
+#define IDC_COMBO1                      1001
+#define IDC_COMBO2                      1002
+#define IDC_BUTTON5                     1006
+#define IDC_EDIT1                       1007
+#define IDC_EDIT2                       1008
+#define IDC_BUTTON8                     1010
+#define IDC_BUTTON9                     1011
+#define IDC_BUTTON10                    1012
+#define IDC_COMBO3                      1013
+#define IDC_BUTTON11                    1014
+#define IDC_COMBO4                      1020
+#define IDC_BUTTON16                    1022
+#define IDC_EDIT3                       1023
+#define IDC_BUTTON17                    1024
+#define IDC_RADIO1                      1025
+#define IDC_RADIO2                      1026
+#define IDC_BUTTON18                    1027
+#define IDC_BUTTON19                    1028
+#define IDC_EDIT4                       1029
+#define IDC_BUTTON20                    1030
+#define IDC_BUTTON21                    1031
+#define IDC_BUTTON22                    1032
+#define IDC_BUTTON23                    1033
+#define IDC_BUTTON24                    1034
+#define IDC_EDIT11                      1042
+#define IDC_EDIT12                      1043
+#define IDC_BUTTON26                    1044
+#define IDC_BUTTON27                    1045
+#define IDC_CHECK2                      1046
+#define IDC_CHECK3                      1047
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1048
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
